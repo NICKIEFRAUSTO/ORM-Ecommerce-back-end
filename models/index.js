ed2861fc1591623ad2e2cb177d65291d448e4f86
Nickie Frausto <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-04-22 22:53:31 -0500

    set associations for product belongsTo category, category hasMany product, product belongsToMany tag, and tag belongsToMany product

--- models/index.js
+++ models/index.js
@@ -1,15 +1,20 @@
 // import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Product = require("./Product");
+const Category = require("./Category");
+const Tag = require("./Tag");
+const ProductTag = require("./ProductTag");
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  // Products belongsTo Category
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  // Categories have many Products
+});
 
+Product.belongsToMany(Tag, { through: ProductTag });
 // Products belongToMany Tags (through ProductTag)
-
+Tag.belongsToMany(Product, { through: ProductTag });
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
