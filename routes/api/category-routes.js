d92a61b7d5bdec42e1ed4d6a757517f1ed244f9b
Nickie Frausto <79732055+NICKIEFRAUSTO@users.noreply.github.com>
2021-04-23 20:05:40 -0500

    added a find by id route to category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -4,30 +4,40 @@ const { findAll, update } = require("../../models/Category");
 
 // The `/api/categories` endpoint
 
+// ------------find all categories be sure to include its associated Products
+//
 router.get("/", (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
   Category.findAll({
     include: [Product],
   })
     .then((categories) => res.json(categories))
     .catch((err) => res.status(500).json(err));
 });
-
+//
+// -----------find one category by its `id` value be sure to include its associated Products
+//
 router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findByPk(
+    req.params.id,
+    {
+    include: [Product],
+    })
+    .then((categoryData) => res.json(categoryData))
+    .catch((err) => res.status(500).json(err));
 });
 
+//
+// -----------create a new category
+//
 router.post("/", (req, res) => {
-  // create a new category
   Category.create(req.body)
     .then((category) => res.status(200).json(category))
     .catch((err) => res.status(500).json(err));
 });
-
+//
+// -----------update a category by its `id` value
+//
 router.put("/:id", async (req, res) => {
-  // update a category by its `id` value
   const categoryData = await Category.update(
     {
       category_name: req.body.category_name,
@@ -39,8 +49,9 @@ router.put("/:id", async (req, res) => {
   return res.json(categoryData);
 });
 
-router.delete("/:id", (req, res) => {
-  // delete a category by its `id` value
-});
+//
+// ------------delete a category by its `id` value
+//
+router.delete("/:id", (req, res) => {});
 
 module.exports = router;
